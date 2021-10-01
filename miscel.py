3d1559b6f7041bfeef3b279691abaaa0b090859f
gullesh <mnourelahi@protonmail.com>
2021-09-30 21:25:41 -0600

    ScoreCamValidated

--- miscel.py
+++ miscel.py
@@ -10,10 +10,10 @@ def bbinfo(bbname):
     root = tree.getroot()
     imwidth = int(root[3][0].text)
     imheight = int(root[3][1].text)
-    xmin = int(root[5][4][0].text)-1
-    ymin = int(root[5][4][1].text)-1
-    xmax = int(root[5][4][2].text)-1
-    ymax = int(root[5][4][3].text)-1
+    xmin = int(root[5][4][0].text)
+    ymin = int(root[5][4][1].text)
+    xmax = int(root[5][4][2].text)
+    ymax = int(root[5][4][3].text)
 
     return imwidth, imheight, xmin, ymin, xmax, ymax
 
@@ -22,16 +22,21 @@ def bbinfo(bbname):
 def findloc(saliency, imwidth, imheight):
     saliency = torch.squeeze(saliency)
     saliency = saliency.cpu().detach().numpy()
-    #salresized = cv2.resize(saliency, (imwidth, imheight))
-    #xloc,yloc = np.unravel_index(salresized.argmax(), salresized.shape)
-    xloc,yloc = np.unravel_index(saliency.argmax(), saliency.shape)
+    salresized = cv2.resize(saliency, (imwidth, imheight))
+    xloc,yloc = np.unravel_index(salresized.argmax(), salresized.shape)
 
     return xloc, yloc
 
 # Constructing groundtruth bounding box for torchray's pointing game 
 def gtbb(imwidth, imheight, xmin, ymin, xmax, ymax):
-
+    a = np.zeros((imheight, imwidth))
+    a[ymin:ymax+1, xmin:xmax+1] = 1
     Y = torch.zeros((imheight, imwidth), dtype=torch.bool)
     Y[ymin:ymax+1, xmin:xmax+1]=True
-    return Y
+    return Y, a
 
+def sares(saliency, imwidth, imheight):
+    saliency = torch.squeeze(saliency)
+    saliency = saliency.cpu().detach().numpy()
+    salresized = cv2.resize(saliency, (imwidth, imheight))
+    return salresized
